2eec51cd926b7ad27e5b9ff5c2770d79c07c3e3a
ivanfeng <ivanfeng0212@gmail.com>
2024-04-18 23:07:51 +0800

    Add KataExercises Test

---
+++ 2.KataExercises/FizzBuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.KataExercises
+{
+    public class FizzBuzz
+    {
+        public string Invoke(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            };
+
+            return number.ToString();
+        }
+    }
+}
---
+++ 99.UnitTest/KataExercises/FizzBuzzTest.cs
@@ -0,0 +1,52 @@
+﻿using _2.KataExercises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99.UnitTest.KataExercises
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        private FizzBuzz _fizzBuzz;
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void GivenFizzBuzz_WhenInputNumber_ThenReturnString(int number)
+        {
+            GivenFizzBuzz();
+
+            ThenReturnString(WhenInputeNumber(number));
+        }
+
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void GivenFizzBuzz_WhenInputNumberIsDivisibleBy3_ThenReturnFizz(int number)
+        {
+            GivenFizzBuzz();
+
+            var result = _fizzBuzz.Invoke(number);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        private void GivenFizzBuzz()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
+        private string WhenInputeNumber(int number)
+        {
+            return _fizzBuzz.Invoke(number);
+        }
+
+        private void ThenReturnString(string fizzBuzzResult)
+        {
+            Assert.That(fizzBuzzResult.GetType(), Is.EqualTo(typeof(string)));
+        }
+    }
+}
