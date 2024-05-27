58c34786f5c1680711a3d59513974caef7f54fed
ivanfeng <ivanfeng0212@gmail.com>
2024-05-27 23:06:16 +0800

    新增 StringCalculator 類別與測試方法

---
+++ 2.KataExercises/StringCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.KataExercises
+{
+    public class StringCalculator
+    {
+        public int Invoke(string inputVal)
+        {
+            if (string.IsNullOrEmpty(inputVal)) return 0;
+
+            if (int.TryParse(inputVal, out var result)) { return result; }
+
+            if (inputVal.Contains("\n"))
+            {
+                inputVal = inputVal.Replace("\n", ",");
+            }
+
+            var stringNumbers = inputVal.Split(',');
+
+            result = 0;
+            foreach (var number in stringNumbers)
+            {
+                result += Convert.ToInt32(number);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 99.UnitTest/KataExercises/StringCalculatorTest.cs
@@ -0,0 +1,70 @@
+﻿using _2.KataExercises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99.UnitTest.KataExercises
+{
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+        private StringCalculator _stringCalculator;
+
+        [SetUp]
+        public void Init()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [TestCase("")]
+        public void GivenEmptyString_WhenInvoke_ThenReturnZero(string inputVal)
+        {
+            ThenReturnZero(InvokeInputVal(inputVal));
+        }
+
+        [TestCase("")]
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("4")]
+        [TestCase("5")]
+        public void GivenStringNumber_WhenInvoke_ThenReturnIntegerNumber(string inputVal)
+        {
+            ThenReturnIntegerNumber(InvokeInputVal(inputVal), inputVal);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("2,3", 5)]
+        [TestCase("3,4", 7)]
+        [TestCase("4,5", 9)]
+        [TestCase("4,5\n6", 15)]
+        public void GivenStringWithTwoNumbers_WhenInvoke_ThenReturnTheirSum(string inputVal, int expected)
+        {
+            ThenEqualToExpected(InvokeInputVal(inputVal), expected);
+        }
+
+        private int InvokeInputVal(string inputVal)
+        {
+            return _stringCalculator.Invoke(inputVal);
+        }
+
+        private static void ThenReturnZero(int result)
+        {
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        private void ThenReturnIntegerNumber(int result, string inputVal)
+        {
+            int.TryParse(inputVal, out int expected);
+
+            Assert.That(result, Is.EqualTo(Convert.ToInt32(expected)));
+        }
+
+        private void ThenEqualToExpected(int result, int expected)
+        {
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
